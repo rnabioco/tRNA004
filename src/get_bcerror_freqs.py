1f2580ad247388d347bfb1cdbcec9782bcacd66e
Laura White <laura.white+github@gmail.com>
2023-12-27 14:31:56 -0700

    more granular bcerror script

--- src/get_bcerror_freqs.py
+++ src/get_bcerror_freqs.py
@@ -10,8 +10,16 @@ def calculate_error_frequencies(bam_file, fasta_file):
 
     for ref in faidx.references:
         ref_len = faidx.get_reference_length(ref)
-        base_calls = [0] * ref_len
-        errors = [0] * ref_len
+        # initialize arrays to store this data with as many 0s as the length of the ref
+        coverage = [0] * ref_len
+        base_counts = {'A': [0] * ref_len, 'T': [0] * ref_len, 
+                       'G': [0] * ref_len, 'C': [0] * ref_len, 'N': [0] * ref_len}
+        mismatches = [0] * ref_len
+        insertions = [0] * ref_len
+        deletions = [0] * ref_len
+        quality_scores = [0] * ref_len # get the Phreds
+        quality_counts = [0] * ref_len  # for averaging later
+
 
         for read in samfile.fetch(ref):
             if read.is_unmapped:
@@ -20,50 +28,70 @@ def calculate_error_frequencies(bam_file, fasta_file):
             ref_pos = read.reference_start
             read_pos = 0
             read_seq = read.query_sequence
+            read_qual = read.query_qualities 
 
             for cigar_op, cigar_len in read.cigartuples:
-                if cigar_op == 0:  # Matches and mismatches
+            # integers fed to read.cigartuples are defined by pysam
+            # cigar_len is the length of each CIGAR operation
+                if cigar_op == 0 or cigar_op == 7 or cigar_op == 8:  # handle all match/mismatch options, including X and =
                     for i in range(cigar_len): # differentiate matches from mismatches
+                        coverage[ref_pos + i] += 1
                         ref_base = faidx.fetch(ref, ref_pos + i, ref_pos + i + 1)
                         read_base = read_seq[read_pos + i]
-                        base_calls[ref_pos + i] += 1
-                        if read_base != ref_base:
-                            errors[ref_pos + i] += 1
+                        qual = read_qual[read_pos + i]
+
+                        # update quality scores
+                        quality_scores[ref_pos + i] += qual
+                        quality_counts[ref_pos + i] += 1
+
+                        # update base counts
+                        if read_base in base_counts:
+                            base_counts[read_base][ref_pos + i] += 1
+
+                        # update mismatches
+                        if read_base != ref_base or cigar_op == 8:
+                            mismatches[ref_pos + i] += 1
+
+                    # increment positions
                     ref_pos += cigar_len
                     read_pos += cigar_len
                 elif cigar_op == 1:  # Insertions
+                    insertions[ref_pos] += cigar_len
                     read_pos += cigar_len
                 elif cigar_op == 2:  # Deletions
+                    deletions[ref_pos:ref_pos + cigar_len] = [x + 1 for x in deletions[ref_pos:ref_pos + cigar_len]]
                     ref_pos += cigar_len
-                elif cigar_op == 3:  # Skipped regions
+                elif cigar_op == 3: # Skips
                     ref_pos += cigar_len
-                elif cigar_op == 4:  # Soft clipped bases
-                    read_pos += cigar_len
-                elif cigar_op == 7: # in the event of = used as match
-                    for i in range(cigar_len):
-                        ref_pos += cigar_len
-                        read_pos += cigar_len
-                elif cigar_op == 8: # in the event of X used as mismatch
-                    for i in range(cigar_len):
-                        errors[ref_pos + i] += 1
-                        base_calls[ref_pos + i] += 1
-                    ref_pos += cigar_len
+                elif cigar_op == 4:  #  Soft clips
                     read_pos += cigar_len
                 else:  # 5 and 6 = Hard clipped bases, padding
                     pass
 
-        # Calculate error frequencies for this reference
-        error_freqs = [errors[i] / base_calls[i] if base_calls[i] > 0 else 0 for i in range(ref_len)]
-        
-        # Store the error frequencies for this reference
-        for pos, freq in enumerate(error_freqs):
-            error_data.append((ref, pos + 1, freq))
+        # Calculate error frequencies and other metrics
+        for pos in range(ref_len):
+            pos_data = {
+                "Reference": ref,
+                "Position": pos + 1,
+                "Coverage": coverage[pos],
+                "A": base_counts['A'][pos],
+                "T": base_counts['T'][pos],
+                "G": base_counts['G'][pos],
+                "C": base_counts['C'][pos],
+                "N": base_counts['N'][pos],
+                "MismatchFreq": mismatches[pos] / coverage[pos] if coverage[pos] > 0 else 0,
+                "InsertionFreq": insertions[pos] / coverage[pos] if coverage[pos] > 0 else 0,
+                "DeletionFreq": deletions[pos] / coverage[pos] if coverage[pos] > 0 else 0,
+                "MeanQual": quality_scores[pos] / quality_counts[pos] if quality_counts[pos] > 0 else 0
+
+            }
+            error_data.append(pos_data)
 
     samfile.close()
     faidx.close()
 
     # Convert the results to a DataFrame
-    err_df = pd.DataFrame(error_data, columns=["Reference", "Position", "ErrorFrequency"])
+    err_df = pd.DataFrame(error_data)
     return err_df
 
 if __name__ == "__main__":
