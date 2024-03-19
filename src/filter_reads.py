6e57f2fe2d62ebaf10a0368b4b572e7c417c7a65
lkwhite <laura.white+github@gmail.com>
2024-03-19 14:28:02 -0600

    accept cl args

--- src/filter_reads.py
+++ src/filter_reads.py
@@ -1,17 +1,23 @@
 #!/usr/bin/env python
 
+
+import sys
 import pysam
 
-# Open the BAM file
-bamfile = pysam.AlignmentFile("your_file.bam", "rb")
+# Use command-line arguments for input and output file names
+input_bam = sys.argv[1]
+output_bam = sys.argv[2]
 
+# Open the BAM file
+bamfile = pysam.AlignmentFile(input_bam, "rb")
+    
 # Adapter lengths
 adapter_5p_len = 24  # Total length of the 5' tRNA adapter
 adapter_3p_len = 30  # Total length of the 3' tRNA adapter
 truncation_max = 15  # Maximum truncation allowed at the 5' end (as helicase loses contact with the RNA)
 
 # Filtering logic to ID "full length" reads that align to adapters but permit 5' end truncation
-with pysam.AlignmentFile("filtered_alignments.bam", "wb", template=bamfile) as outfile:
+with pysam.AlignmentFile(output_bam, "wb", template=bamfile) as outfile:
     for read in bamfile:
         # Assuming the reference sequence includes the 5' adapter, RNA body, and 3' adapter,
         # and that the adapters are at the very beginning and end of the reference:
